5ee1d503e122809d8c5e7d7954b4201df5f4671d
casperj <149433586+casperjdev@users.noreply.github.com>
2025-06-19 22:34:19 +0200

    Update AboutPrimary.svelte

--- src/components/ui/atoms/AboutPrimary.svelte
+++ src/components/ui/atoms/AboutPrimary.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <ScrollObserver className="xl:px-12 grid gap-12 place-items-center grid-flow-row lg:grid-flow-col" {transition}>
-	<span class="w-32 h-full min-h-32 [background-image:url('/author.png')]"></span>
+	<span class="w-32 h-full min-h-32 [background-image:url('/author.jpg')]"></span>
 	<h1 class="text-red-400 text-[clamp(0.5rem,2vw,1rem)]">
 		I picked up programming 5 years ago and ever since then web development has been one of my main hobbies, which later
 		turned into a career choice. Over that time period I've obtained a bunch of skills and picked up good coding habits.
